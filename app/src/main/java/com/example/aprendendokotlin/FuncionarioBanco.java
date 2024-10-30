250331f9acf331ad2e36438c1f6ad1a807eccfcd
Allison Cristiano Boita <61565817+AllisonBoita@users.noreply.github.com>
2024-10-30 13:28:17 -0300

    Programação Orientada a Objetos

---
+++ app/src/main/java/com/example/aprendendokotlin/FuncionarioBanco.java
@@ -0,0 +1,10 @@
+package com.example.aprendendokotlin;
+
+public class FuncionarioBanco {
+    double salario = 0;
+
+    // Metodo
+    void calcularSalarioComDesconto(){ // Metodo sem retorno - Executa algo
+
+    }
+}
